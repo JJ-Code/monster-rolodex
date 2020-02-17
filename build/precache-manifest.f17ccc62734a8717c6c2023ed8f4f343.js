a89748d41e7958b4d55287e55f80007812d1c270
Jaymee Liu <{jaymeeliu@gmail.com}>
2020-02-16 17:30:10 -0800

    upload project

---
+++ build/precache-manifest.f17ccc62734a8717c6c2023ed8f4f343.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "721cc77dd4239a064d318f48ee6766e9",
+    "url": "/JJ-Code/monster-rolodex/index.html"
+  },
+  {
+    "revision": "adb0fbaebad6c85d501d",
+    "url": "/JJ-Code/monster-rolodex/static/css/main.06a37190.chunk.css"
+  },
+  {
+    "revision": "5972ab9a24d87de2b217",
+    "url": "/JJ-Code/monster-rolodex/static/js/2.40aee04e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/JJ-Code/monster-rolodex/static/js/2.40aee04e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adb0fbaebad6c85d501d",
+    "url": "/JJ-Code/monster-rolodex/static/js/main.a6dd2e53.chunk.js"
+  },
+  {
+    "revision": "508797e49dd5c5e52233",
+    "url": "/JJ-Code/monster-rolodex/static/js/runtime-main.03840481.js"
+  }
+]);
---
+++ src/App.js
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+import './App.css';
+import {CardList} from "./component/card-list/card-list" 
+import { SearchBox } from "./component/search-box/search-box" 
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchField: "",
+      title: ''
+    }
+    this.handleChange = this.handleChange.bind(this)
+
+  }
+  handleChange(e){
+    this.setState({ searchField: e.target.value })
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({monsters: users}))
+
+  }
+  render() {
+    const {monsters, searchField} = this.state;
+    const filterMonsters = monsters.filter(monster => 
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    )
+
+
+    return (
+      <div className="App">
+        {/* <SearchBox placeholder='Search Monsters' handleChange={e =>
+          this.setState({ searchField: e.target.value })}/> */}
+        <h1>Monsters Rolodex</h1>
+        <SearchBox placeholder='Search Monsters' handleChange={this.handleChange} />
+        <CardList monsters={filterMonsters} />
+      </div>
+    );
+  } 
+}
+
+
+
+
+
+
+
+
+export default App;
+
+
+
+
